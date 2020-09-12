8d04fb12e6f6f40e54067f0f93ee3aa7158e92ac
mabuni741 <mabuni741@gmail.com>
2020-09-12 19:05:37 +0530

    My second file

---
+++ mul.py
@@ -0,0 +1,23 @@
+# a=5
+# for i in range(1,a+1):
+#     x=5
+#     for j in range(1,i+1):
+#         print(x,end=" ")
+#         x-=1
+# print()
+  
+# a=int(input("enter a value:"))
+# for i in range(1,a+1):
+#     x=5
+#     for j in range(1,i+1):
+#         print(x,end=" ")
+#         x+=1
+# print()
+
+a=int(input("enter a value:"))
+for i in range(1,a+1):
+    x=i
+    for j in range(1,i*1):
+        print("a","*",i,"=" a*i)
+        #x*=1
+print()
